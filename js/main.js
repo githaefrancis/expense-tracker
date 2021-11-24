4fb5a9754fb8917b86dafe332ebbdc0ee4f23322
Francis Githae <francis.githae@student.moringaschool.com>
2021-11-24 22:07:35 +0300

    add remove item functionality

--- js/main.js
+++ js/main.js
@@ -9,27 +9,33 @@ let totalCost = 0;
 //get the form values
 $(() => {
   $("[name=date]").val("2021-11-05");
-  // alert(new Date($.now()).toDateString("YY-MM-DD"));
+  // alert(new Date($.now()).toLocaleDateString());
   let expenseCount = 0;
   $("form").submit((e) => {
     e.preventDefault();
     let expenseDate = $("[name=date]").val();
     let expenseDescription = $("[name=description]").val();
     let expenseAmount = $("[name=amount]").val();
+
+    date = new Date(expenseDate);
+
     expenseCount++;
 
     expenses.push(
       new Expense(expenseCount, expenseDate, expenseDescription, expenseAmount)
     );
     totalCost += parseInt(expenseAmount);
-    alert(totalCost);
     $("#expense-list").append(`<tr>
-    <th scope="row">1</th>
-    <td>24/11/2021</td>
+    <th scope="row" id="${expenses[expenses.length - 1].index}">1</th>
+    <td>${expenses[expenses.length - 1].date}</td>
     <td>${expenses[expenses.length - 1].description}</td>
     <td>${expenses[expenses.length - 1].amount}</td>
+    <td><i class="fa fa-trash-o fa-lg "></i></td>
   </tr>`);
-      //show total
-    $('.total').text(totalCost);
+    //show total
+    $(".total").text(+totalCost);
+    $(".fa").click((e) => {
+      e.target.parentElement.parentElement.remove();
+    });
   });
 });
