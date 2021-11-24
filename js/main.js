2514831d883aaf1e6e5298132787fc2d360d5d6e
Francis Githae <francis.githae@student.moringaschool.com>
2021-11-24 19:48:20 +0300

    display total

--- js/main.js
+++ js/main.js
@@ -1,30 +1,35 @@
-function Expense(index,date, description, amount) {
-  this.index=index;
+function Expense(index, date, description, amount) {
+  this.index = index;
   this.date = date;
   this.description = description;
   this.amount = amount;
 }
-let expenses=[];
-
+let expenses = [];
+let totalCost = 0;
 //get the form values
-$(()=>{
+$(() => {
   $("[name=date]").val("2021-11-05");
   // alert(new Date($.now()).toDateString("YY-MM-DD"));
-  let expenseCount=0;
+  let expenseCount = 0;
   $("form").submit((e) => {
     e.preventDefault();
     let expenseDate = $("[name=date]").val();
-    // alert(expenseDate);
     let expenseDescription = $("[name=description]").val();
     let expenseAmount = $("[name=amount]").val();
     expenseCount++;
-    expenses.push(new Expense(expenseCount,expenseDate,expenseDescription,expenseAmount));
-    console.log(expenses);
-    $('#expense-list').append(`<tr>
+
+    expenses.push(
+      new Expense(expenseCount, expenseDate, expenseDescription, expenseAmount)
+    );
+    totalCost += parseInt(expenseAmount);
+    alert(totalCost);
+    $("#expense-list").append(`<tr>
     <th scope="row">1</th>
     <td>24/11/2021</td>
-    <td>${expenses[expenses.length-1].description}</td>
-    <td>${expenses[expenses.length-1].amount}</td>
+    <td>${expenses[expenses.length - 1].description}</td>
+    <td>${expenses[expenses.length - 1].amount}</td>
   </tr>`);
+      //show total
+    $('.total').text(totalCost);
   });
 });
